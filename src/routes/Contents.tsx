dbc2577f1161e9779ff85568e59aecdf12e175f8
Celeaxy <erich.sartison@web.de>
2022-03-31 14:12:44 +0000

    updated readme url

--- src/routes/Contents.tsx
+++ src/routes/Contents.tsx
@@ -1,6 +1,6 @@
 import Content from "../components/Content";
 
 const Documentation = <Content file='Docs.md' markdown/>;
-const ReadMe = <Content file='https://raw.githubusercontent.com/SebastianBrack/juri/master/README.md' markdown external />;
+const ReadMe = <Content file='https://raw.githubusercontent.com/SebastianBrack/JuriLang/master/README.md' markdown external />;
 
 export {Documentation, ReadMe};
